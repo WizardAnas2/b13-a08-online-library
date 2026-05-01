7c896a36646aa4468eac67953f4dd7ce8fdc7cda
WizardAnas2 <mdsaadi1810@gmail.com>
2026-05-01 20:38:01 +0600

    added some changes with deployment issue

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
     
-    baseURL: "http://localhost:3000" 
+    baseURL: "https://b13-a08-online-library.vercel.app" 
 });
